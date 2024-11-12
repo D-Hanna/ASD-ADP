c7a701cd2714736c9fcb859c3ff8b585a8268daf
david <david@DAVIPC>
2024-11-12 21:17:02 +0100

    Add project files.

---
+++ ASD_ADP/Algorithm1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASD_ADP
+{
+    public static class Algorithm1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("holy shit het werkt");
+        }
+    }
+}
---
+++ ASD_ADP/Program.cs
@@ -0,0 +1,18 @@
+﻿using ASD_ADP;
+
+Console.WriteLine("Hello, World!");
+var algorithm = Console.ReadLine();
+
+switch (algorithm)
+{
+    case ("testing"):
+        {
+            Algorithm1.Main(args);
+            break;
+        }
+        default:
+        {
+            Console.WriteLine("could not find this algorithm");
+            break;
+        }
+}
